f6d4f852e5e16981edb09e2633c05818d83ee585
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2024-11-15 10:37:11 -0300

    :recycle: update emacs/config/core/settings.el

--- emacs/config/core/settings.el
+++ emacs/config/core/settings.el
@@ -4,26 +4,21 @@
 
 ;;; Code:
 
-(setq-default electric-indent-inhibit t)  ;; Disable electric indentation
-(setq ring-bell-function 'ignore)          ;; Disable the bell sound
-(setq select-enable-clipboard t)            ;; Enable clipboard selection
-(setq-default cursor-type 'bar)             ;; Set cursor type to bar
-(setq scroll-step 1                         ;; Set scrolling step
-      scroll-conservatively 10000)          ;; Scroll conservatively
-(setq inhibit-startup-message t)            ;; Disable the startup message
+(setq-default electric-indent-inhibit t)
 (setq ring-bell-function 'ignore)
+(setq select-enable-clipboard t)
+(setq-default cursor-type 'bar)
+(setq scroll-step 1
+      scroll-conservatively 10000)
+(setq inhibit-startup-message t)
+(setq ring-bell-function 'ignore)
+(setq save-interprogram-paste-before-kill nil)
 
-;; no backup files
+
 (setq make-backup-files nil)
-
-(setq backup-directory-alist '((".*" . "~/.saves")))
-
-;; no lock files
 (setq create-lockfiles nil)
-
-;; automatically update the file if there are any changes
+(setq backup-directory-alist '((".*" . "~/.saves")))
 (global-auto-revert-mode t)
-
 (setq-default truncate-lines t)
 
 ;;; settings.el ends here
