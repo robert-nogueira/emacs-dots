7c1e368112517cea626353729a24d75e1ace84f9
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2024-11-15 11:00:26 -0300

    :recycle: change emacs theme

--- emacs/config/ui/theme.el
+++ emacs/config/ui/theme.el
@@ -5,12 +5,7 @@
 ;;; Code:
 
 (use-package catppuccin-theme :ensure t)
-(use-package autothemer :ensure t)
-
-;;; Install theme from GitHub
-(straight-use-package
- '(rose-pine-emacs :host github :repo "thongpv87/rose-pine-emacs" :branch "master"))
-(load-theme 'rebecca t)
+(load-theme 'doom-moonlight t)
 
 (provide 'theme)
 ;;; theme.el ends here
