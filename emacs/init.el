628a24564ab1a9662205d7f80000cadd103d2095
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2024-11-15 23:04:33 -0300

    :recycle: refactored init.el

--- emacs/init.el
+++ emacs/init.el
@@ -56,7 +56,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "7bcbee7c5eaef0eca0dad6c5ec0ee8f665efd857be421a67be1ce84003d71417" "11feb87b02688866cef2199e268cad5f6d473ebacaa5f06c35c3ac08894a2845" default)))
+   '("0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "71ef2c8ced402aa3cd319a799e748631a1c080a8aeb0852469c589b59547be76" "4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "d481904809c509641a1a1f1b1eb80b94c58c210145effc2631c1a7f2e4a2fdf4" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "7bcbee7c5eaef0eca0dad6c5ec0ee8f665efd857be421a67be1ce84003d71417" "11feb87b02688866cef2199e268cad5f6d473ebacaa5f06c35c3ac08894a2845" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
