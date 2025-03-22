116dc20c956ef94aebdbc8b10b15312623e4d068
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-03-22 02:34:55 -0300

    chore(python.el): comment out lsp-diagnostic-package setting to prevent disabling diagnostics feat(python.el): set lsp-pyright-venv-path to use poetry environment path for better integration style(python.el): add custom face for unused variables to enhance code visibility

--- emacs/config/programming/python.el
+++ emacs/config/programming/python.el
@@ -33,7 +33,7 @@
   :hook ((python-mode . lsp))
   :config
   (setq lsp-headerline-breadcrumb-enable nil)
-  (setq lsp-diagnostic-package :none)
+  ;; (setq lsp-diagnostic-package :none)
   (setq lsp-enable-snippet nil))
 
 (add-hook 'python-mode-hook (lambda () (setqsetq-local company-backends '(company-capf company-dabbrev))))
@@ -43,6 +43,7 @@
   :hook
   ((python-mode . (lambda ()
                     (require 'lsp-pyright)
+                    (setq lsp-pyright-venv-path (string-trim (shell-command-to-string "poetry env info -p")))
                     (lsp-deferred)))))
 
 (advice-add 'lsp--info :around (lambda (&rest _) nil))
@@ -66,7 +67,7 @@
         lsp-ui-doc-delay 0
         ;; lsp-ui-doc-max-width 200
         lsp-ui-doc-max-height 70
-        lsp-ui-doc-border "#cba6f7")  ;; Cor da borda
+        lsp-ui-doc-border "#cba6f7")
 
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
@@ -83,7 +84,9 @@
 (add-hook 'python-mode-hook #'font-lock-mode)
 (setq lsp-disabled-clients '(semgrep-ls))
 (add-hook 'python-mode-hook 'display-line-numbers-mode)
-
+(custom-set-faces
+  '(font-lock-unused-variable-face ((t (:foreground "#89dceb" :weight bold))))
+)
 (with-eval-after-load 'lsp-mode
   (define-key lsp-mode-map (kbd "C-c r") #'lsp-rename))
 (provide 'python)
