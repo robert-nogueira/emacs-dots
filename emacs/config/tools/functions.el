77f1426d9b4997dfebf1f5c251e0abc9ead02f61
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-03-22 02:35:47 -0300

    feat(functions.el): enhance ruff formatting function to check for python-mode before execution fix(functions.el): ensure ruff formatting hook is local to the current buffer only

--- emacs/config/tools/functions.el
+++ emacs/config/tools/functions.el
@@ -7,24 +7,27 @@
 ;;; Code:
 
 (defun my/ruff-format-buffer ()
-  "Format the buffer with ruff and restore the cursor position."
+  "Format the buffer with ruff and restore the cursor position.
+Only runs in python-mode."
   (interactive)
-  (let ((saved-point (point)))
-    (let ((exit-code (call-process-region (point-min) (point-max)
-                                          "ruff" nil "*Ruff Format Output*"
-                                          t "format" "-")))
-      (if (zerop exit-code)
-          (progn
-            (shell-command-on-region (point-min) (point-max) "ruff format -"
-                                     nil t)
-            (goto-char saved-point)
-            (message "Buffer formatted and saved with ruff!"))
-        (message "Error formatting with ruff! Exit code: %d" exit-code)))))
+  (if (derived-mode-p 'python-mode)  ; Check if in python-mode
+      (let ((saved-point (point)))
+        (let ((exit-code (call-process-region (point-min) (point-max)
+                                              "ruff" nil "*Ruff Format Output*"
+                                              t "format" "-")))
+          (if (zerop exit-code)
+              (progn
+                (shell-command-on-region (point-min) (point-max) "ruff format -"
+                                         nil t)
+                (goto-char saved-point)
+                (message "Buffer formatted and saved with ruff!"))
+            (message "Error formatting with ruff! Exit code: %d" exit-code))))
+    (message "Not in Python mode. Ruff formatting skipped.")))
 
 (defun my/setup-ruff-formatting ()
   "Setup keybindings and hooks for Ruff formatting in Python mode."
   (local-set-key (kbd "C-M-l") 'my/ruff-format-buffer)
-  (add-hook 'before-save-hook 'my/ruff-format-buffer))
+  (add-hook 'before-save-hook 'my/ruff-format-buffer nil t))  ; Hook only for current buffer
 
 (defun my/delete-word (arg)
   "Delete characters forward until encountering the end of a word.
@@ -46,14 +49,11 @@ This command does not push text to kill-ring'."
 
 (global-set-key (kbd "M-d") 'my/delete-word)
 (global-set-key (kbd "<M-backspace>") 'my/backward-delete-word)
-
 (global-set-key (kbd "C-d") 'my/delete-word)
 (global-set-key (kbd "<C-backspace>") 'my/backward-delete-word)
+(global-set-key (kbd "M-;") 'comment-line)
 
 (add-hook 'python-mode-hook 'my/setup-ruff-formatting)
-(global-set-key (kbd "C-M-l") 'my/ruff-format-buffer)
-(add-hook 'before-save-hook 'my/ruff-format-buffer)
-(global-set-key (kbd "M-;") 'comment-line)
 
 (provide 'functions)
 ;;; functions.el ends here
