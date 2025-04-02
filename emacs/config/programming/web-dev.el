dd016cc0091d3856e06788cb7a84131f4aac3e5f
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-04-02 05:35:16 -0300

    feat(web-dev.el): enhance web-mode configuration with additional features and hooks for better development experience feat(web-dev.el): add support for typescript-mode and javascript-mode with flycheck integration feat(web-dev.el): integrate tide for TypeScript development and enable prettier for code formatting style(web-dev.el): improve code organization and readability by grouping related configurations together

--- emacs/config/programming/web-dev.el
+++ emacs/config/programming/web-dev.el
@@ -1,33 +1,42 @@
 ;;; web-dev.el --- Web Mode Configuration
 ;;; Commentary:
 
-;; This file configures Web Mode, enabling auto-pairing,
-;; current element highlighting, and autocomplete sources.
+;; Configuração para Web Mode, incluindo auto-pairing,
+;; realce do elemento atual e fontes de autocompletar.
 
 ;;; Code:
 
 (use-package web-mode
   :ensure t
+  :mode (("\.html?\'" . web-mode)
+         ("\.tsx\'" . web-mode)
+         ("\.jsx\'" . web-mode))
   :config
-  (setq web-mode-enable-auto-pairing t)
-  (setq web-mode-enable-current-element-highlight t)
-  (setq web-mode-markup-indent-offset 2)
-  :mode (("\\.js\\'" . web-mode)
-	 ("\\.jsx\\'" .  web-mode)
-	 ("\\.ts\\'" . web-mode)
-	 ("\\.tsx\\'" . web-mode)
-	 ("\\.html\\'" . web-mode))
-  :commands web-mode)
-  (setq web-mode-ac-sources-alist
-        '(("css" . (ac-source-css-property))
-          ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
-
-(flycheck-add-mode 'javascript-eslint 'web-mode)
-
-(use-package json-mode
-  :ensure t)
+  (setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2
+        web-mode-block-padding 2
+        web-mode-comment-style 2
+        web-mode-enable-css-colorization t
+        web-mode-enable-auto-pairing t
+        web-mode-enable-comment-keywords t
+        web-mode-enable-current-element-highlight t)
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                (setup-tide-mode)))))
 
 (use-package typescript-mode
+  :ensure t
+  :config
+  (setq typescript-indent-level 2)
+  (add-hook 'typescript-mode #'subword-mode)
+  (add-hook 'typescript-mode-hook 'flycheck-mode))
+
+(use-package javascript-mode
+  :hook (javascript-mode . flycheck-mode))
+
+(use-package json-mode
   :ensure t)
 
 (use-package prettier-js
@@ -35,20 +44,26 @@
   :hook ((typescript-mode . prettier-js-mode)
          (web-mode . prettier-js-mode)))
 
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
+
 (use-package lsp-tailwindcss
   :straight '(lsp-tailwindcss :type git :host github :repo "merrickluo/lsp-tailwindcss")
-  :init (setq lsp-tailwindcss-add-on-mode t)
+  :after lsp-mode
+  :init
+  (setq lsp-tailwindcss-add-on-mode t
+        lsp-tailwindcss-experimental-class-regex ["tw([^]*)" "tw=\"([^\"]*)" "tw={\"([^\"}]*)" "tw\\.\\w+([^]*)" "tw\\(.*?\\)([^]*)"])
   :config
-  (dolist (tw-major-mode
-           '(css-mode
-             css-ts-mode
-             typescript-mode
-             typescript-ts-mode
-             tsx-ts-mode
-             js2-mode
-             js-ts-mode
-             clojure-mode))
-    (add-to-list 'lsp-tailwindcss-major-modes tw-major-mode)))
+  (dolist (mode '(css-mode css-ts-mode typescript-mode typescript-ts-mode tsx-ts-mode js2-mode js-ts-mode clojure-mode))
+    (add-to-list 'lsp-tailwindcss-major-modes mode)))
+
+(use-package prettier
+  :ensure t)
+(add-hook 'after-init-hook #'global-prettier-mode)
 
 (provide 'web-dev)
 ;;; web-dev.el ends here
