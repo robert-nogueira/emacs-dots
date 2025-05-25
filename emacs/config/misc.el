52477f8473ee3d0df3fd90de447857891bb50a3f
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-05-25 07:53:50 -0300

    ✨ feat(misc.el): add hl-todo package to highlight TODO keywords in code for better visibility

--- emacs/config/misc.el
+++ emacs/config/misc.el
@@ -112,24 +112,35 @@
 (add-to-list 'aggressive-indent-excluded-modes 'html-mode)
 
 (use-package highlight-indent-guides
- :custom
- (highlight-indent-guides-auto-enabled nil)
- (highlight-indent-guides-method 'character)
- (highlight-indent-guides-character ?ǀ) ;; Credit: https://github.com/SwiftyChicken/dotfiles/blob/7cb2f117d722300d36ce4d0e4ad205f7cb758275/.config/emacs/local/etc/code/prog.el#L21
- (highlight-indent-guides-responsive 'stack)
- :hook
- (prog-mode . highlight-indent-guides-mode)
- :config
- (set-face-attribute 'highlight-indent-guides-odd-face nil :background "#7c6fbe")
- (set-face-attribute 'highlight-indent-guides-even-face nil :background "#7c6fbe")
- (set-face-attribute 'highlight-indent-guides-character-face nil :foreground "#7c6fbe")
- (set-face-attribute 'highlight-indent-guides-stack-odd-face nil :background "#7c6fbe")
- (set-face-attribute 'highlight-indent-guides-stack-even-face nil :background "#7c6fbe")
- (set-face-attribute 'highlight-indent-guides-stack-character-face nil :foreground "#7c6fbe")
- (set-face-attribute 'highlight-indent-guides-top-odd-face nil :background "#9ab4ff")
- (set-face-attribute 'highlight-indent-guides-top-even-face nil :background "#9ab4ff")
- (set-face-attribute 'highlight-indent-guides-top-character-face nil :foreground "#9ab4ff")
- )
+  :custom
+  (highlight-indent-guides-auto-enabled nil)
+  (highlight-indent-guides-method 'character)
+  (highlight-indent-guides-character ?ǀ) ;; Credit: https://github.com/SwiftyChicken/dotfiles/blob/7cb2f117d722300d36ce4d0e4ad205f7cb758275/.config/emacs/local/etc/code/prog.el#L21
+  (highlight-indent-guides-responsive 'stack)
+  :hook
+  (prog-mode . highlight-indent-guides-mode)
+  :config
+  (set-face-attribute 'highlight-indent-guides-odd-face nil :background "#7c6fbe")
+  (set-face-attribute 'highlight-indent-guides-even-face nil :background "#7c6fbe")
+  (set-face-attribute 'highlight-indent-guides-character-face nil :foreground "#7c6fbe")
+  (set-face-attribute 'highlight-indent-guides-stack-odd-face nil :background "#7c6fbe")
+  (set-face-attribute 'highlight-indent-guides-stack-even-face nil :background "#7c6fbe")
+  (set-face-attribute 'highlight-indent-guides-stack-character-face nil :foreground "#7c6fbe")
+  (set-face-attribute 'highlight-indent-guides-top-odd-face nil :background "#9ab4ff")
+  (set-face-attribute 'highlight-indent-guides-top-even-face nil :background "#9ab4ff")
+  (set-face-attribute 'highlight-indent-guides-top-character-face nil :foreground "#9ab4ff")
+  )
+
+(use-package hl-todo
+  :ensure t
+  :hook (prog-mode . hl-todo-mode)
+  :config
+  (setq hl-todo-keyword-faces
+        '(("TODO"   . "#f38ba8")
+          ("FIXME"  . "#f38ba8")
+          ("DEBUG"  . "#cba6f7")
+          ("GOTCHA" . "#fab387")
+          ("STUB"   . "#89b4fa"))))
 
 (provide 'misc)
 ;;; misc.el ends here
