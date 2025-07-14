a7314d0083fc6d51520b26ab60aa9b776daa9c1a
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-07-14 03:14:51 -0300

    ✨ feat(vterm.el): enhance vterm-toggle binding to open terminal at project root or default directory for better usability

--- config/tools/vterm.el
+++ config/tools/vterm.el
@@ -13,9 +13,18 @@
 
 (use-package vterm-toggle
   :ensure t
-  :bind (("C-c t" . vterm-toggle)
+  :bind (("C-c t" . my/vterm-toggle-project-root-or-default)
          ("C-c j" . vterm-toggle-forward)
          ("C-c l" . vterm-toggle-backward)))
+
+(defun my/vterm-toggle-project-root-or-default ()
+  (interactive)
+  (let ((default-directory
+         (or (and (fboundp 'projectile-project-root)
+                  (projectile-project-root))
+             default-directory)))
+    (vterm-toggle)))
+
 
 (provide 'vterm)
 ;;; vterm.el ends here
