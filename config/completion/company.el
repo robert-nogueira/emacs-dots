6f55f1057e3c3ea2ed6559c1d1bc1c86936875f5
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-08-01 14:17:40 -0300

    🧹 chore(company.el): remove unused face settings to clean up configuration and improve maintainability

--- config/completion/company.el
+++ config/completion/company.el
@@ -28,14 +28,6 @@
 
 (global-set-key (kbd "s-SPC") 'company-complete)
 
-
-(set-face-foreground 'company-tooltip-annotation "#cba6f7")
-(set-face-foreground 'company-tooltip-annotation-selection "#cba6f7")
-;; (set-face-background 'company-tooltip "#11111b80")
-(set-face-foreground 'company-tooltip "#89b4fa")
-(set-face-background 'company-preview "#11111b")
-(set-face-foreground 'company-preview "#cba6f7")
-
 (setq pos-tip-foreground-color "#89b4fa"
       pos-tip-background-color "#11111b")
 
