cba3dcc1835e5d4a46c9ec3e8820d54f008ee55c
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-08-01 13:24:21 -0300

    🧹 chore(settings.el): remove desktop save mode configuration to simplify settings and avoid unnecessary complexity

--- config/core/settings.el
+++ config/core/settings.el
@@ -24,12 +24,5 @@
 (setq redisplay-dont-pause t)
 (setq vc-follow-symlinks t)
 
-(desktop-save-mode 1)
-(unless (file-directory-p "~/.emacs.d/desktop/")
-  (make-directory "~/.emacs.d/desktop/" t))
-(setq desktop-path '("~/.emacs.d/desktop/"))
-(setq desktop-dirname "~/.emacs.d/desktop/")
-(setq desktop-base-file-name "emacs-desktop")
-
 ;;; settings.el ends here
 (provide 'settings)
