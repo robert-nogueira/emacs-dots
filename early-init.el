15740c1cd4b59aa06a8d61d8de2070c9da6d41d2
Robert <contact@robertnogueira.com>
2026-01-03 04:56:29 -0300

    ✨ feat(emacs): add early-init.el and update .gitignore to include it

---
+++ early-init.el
@@ -0,0 +1,11 @@
+(setq package-enable-at-startup nil)
+(setq gc-cons-threshold most-positive-fixnum)
+(setq read-process-output-max (* 1024 1024))
+
+(setq default-frame-alist
+      '((background-color . "#212337")
+        (foreground-color . "#c0caf5")
+        (cursor-color     . "#c0caf5")))
+
+(add-to-list 'initial-frame-alist '(background-color . "#212337"))
+(add-to-list 'initial-frame-alist '(foreground-color . "#c0caf5"))
