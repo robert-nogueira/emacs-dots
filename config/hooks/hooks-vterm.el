65a564f3684daad4c9142461809986158fa5aaaf
Robert <contact@robertnogueira.com>
2026-01-29 21:10:06 -0300

    ✨ feat(vterm): disable mode line and clean up buffer in vterm-mode hook

--- config/hooks/hooks-vterm.el
+++ config/hooks/hooks-vterm.el
@@ -1,15 +1,15 @@
 ;;; hooks-vterm.el --- Vterm hook configuration
-
 ;;; Commentary:
-;; Disable line numbers in vterm-mode.
-
+;; Disable line numbers and mode line in vterm-mode.
 ;;; Code:
 
 (with-eval-after-load 'vterm
-  (defun my/vterm-disable-line-numbers ()
-    (display-line-numbers-mode -1))
+  (defun my/vterm-cleanup ()
+    "Disable line numbers, mode line and centaur-tabs in vterm buffer."
+    (display-line-numbers-mode -1)
+    (setq-local mode-line-format nil))
 
-  (add-hook 'vterm-mode-hook #'my/vterm-disable-line-numbers))
+  (add-hook 'vterm-mode-hook #'my/vterm-cleanup))
 
 (provide 'hooks-vterm)
 ;;; hooks-vterm.el ends here
