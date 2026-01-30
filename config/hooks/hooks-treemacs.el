026d625252a5ea7f6d50f1ec3844fb2ee4514ea7
Robert <contact@robertnogueira.com>
2026-01-29 21:08:42 -0300

    ✨ feat(treemacs): disable line numbers and hide mode line in treemacs buffers

--- config/hooks/hooks-treemacs.el
+++ config/hooks/hooks-treemacs.el
@@ -1,15 +1,19 @@
 ;;; hooks-treemacs.el --- Treemacs hook configuration
-
 ;;; Commentary:
-;; Disable line numbers in treemacs-mode.
-
+;; Disable line numbers and the mode line only in treemacs-mode.
 ;;; Code:
 
 (with-eval-after-load 'treemacs
-  (defun my/treemacs-disable-line-numbers ()
-    (display-line-numbers-mode -1))
+  (defun my/treemacs-cleanup ()
+    "Disable line numbers and hide the mode line only in Treemacs buffer."
+    (when (eq major-mode 'treemacs-mode)
+      (display-line-numbers-mode -1)
+      (setq mode-line-format nil)))
 
-  (add-hook 'treemacs-mode-hook #'my/treemacs-disable-line-numbers))
+  (add-hook 'treemacs-mode-hook #'my/treemacs-cleanup)
+
+  ;; Garantir que aconteça após todas as inicializações
+  (add-hook 'after-change-major-mode-hook #'my/treemacs-cleanup))
 
 (provide 'hooks-treemacs)
 ;;; hooks-treemacs.el ends here
