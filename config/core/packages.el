49ba464a103e57606513f7e151f6233121f7d9e8
Robert Nogueira <contact@robertnogueira.com>
2026-06-25 18:12:50 -0300

    🐛 fix(packages): disable auto-update prompt to avoid blocking daemon

--- config/core/packages.el
+++ config/core/packages.el
@@ -28,7 +28,7 @@
 (use-package auto-package-update
   :custom
   (auto-package-update-interval 7)
-  (auto-package-update-prompt-before-update t)
+  (auto-package-update-prompt-before-update nil)
   (auto-package-update-hide-results t)
   :config
   (auto-package-update-maybe)
